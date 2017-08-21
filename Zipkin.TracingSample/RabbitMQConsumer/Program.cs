c402f4a4a3489741b5bdae6c276a7a036668b68b
Vasili Puchko <vasili.puchko@aerdata.com>
2017-08-22 00:27:14 +0200

    added zipkin http client integration and created initial samples structure

---
+++ Zipkin.TracingSample/RabbitMQConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Zipkin/Zipkin.Http/HttpRequestMessageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Zipkin.XB3Propagation;
+
+namespace Zipkin.Http
+{
+    public static class HttpRequestMessageExtensions
+    {
+	    public static void EnableZipkinTrace(this HttpRequestMessage requestMessage)
+	    {
+		    var headers = requestMessage.Headers;
+
+		    var zipkinHeaders = new Dictionary<string, string>(8);
+			zipkinHeaders.WriteX3BHeaders();
+
+		    if (zipkinHeaders.Count == 0)
+		    {
+			    return;
+		    }
+
+		    foreach (var zipkinHeader in zipkinHeaders)
+		    {
+			    headers.Add(zipkinHeader.Key, zipkinHeader.Value);
+		    }
+	    }
+    }
+}
